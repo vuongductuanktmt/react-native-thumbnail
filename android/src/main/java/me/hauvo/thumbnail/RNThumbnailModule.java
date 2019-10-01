4750041897505b7f9acfbf8a77639aeb6ee3ce17
vuongductuanktmt <anhtuanpro312@gmail.com>
2019-10-02 00:28:58 +0700

    Update RNThumbnailModule.java

--- android/src/main/java/me/hauvo/thumbnail/RNThumbnailModule.java
+++ android/src/main/java/me/hauvo/thumbnail/RNThumbnailModule.java
@@ -8,15 +8,20 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
+
+import android.graphics.BitmapFactory;
 import android.media.ThumbnailUtils;
 import android.provider.MediaStore;
 import android.provider.MediaStore.Video.Thumbnails;
 import android.graphics.Bitmap;
 import android.os.Environment;
+import android.util.Base64;
 import android.util.Log;
 import android.media.MediaMetadataRetriever;
-import 	android.graphics.Matrix;
+import android.graphics.Matrix;
 
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
 import java.util.UUID;
 import java.io.File;
 import java.io.OutputStream;
@@ -25,58 +30,60 @@ import java.io.FileOutputStream;
 
 public class RNThumbnailModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNThumbnailModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
-
-  @Override
-  public String getName() {
-    return "RNThumbnail";
-  }
-
-  @ReactMethod
-  public void get(String filePath, Promise promise) {
-    filePath = filePath.replace("file://","");
-    MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-    retriever.setDataSource(filePath);
-    Bitmap image = retriever.getFrameAtTime(1000000, MediaMetadataRetriever.OPTION_CLOSEST_SYNC);
-
-    String fullPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/thumb";
-
-    try {
-      File dir = new File(fullPath);
-      if (!dir.exists()) {
-        dir.mkdirs();
-      }
-
-      OutputStream fOut = null;
-      // String fileName = "thumb-" + UUID.randomUUID().toString() + ".jpeg";
-      String fileName = "thumb-" + UUID.randomUUID().toString() + ".jpeg";
-      File file = new File(fullPath, fileName);
-      file.createNewFile();
-      fOut = new FileOutputStream(file);
-
-      // 100 means no compression, the lower you go, the stronger the compression
-      image.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
-      fOut.flush();
-      fOut.close();
-
-      // MediaStore.Images.Media.insertImage(reactContext.getContentResolver(), file.getAbsolutePath(), file.getName(), file.getName());
-
-      WritableMap map = Arguments.createMap();
-
-      map.putString("path", "file://" + fullPath + '/' + fileName);
-      map.putDouble("width", image.getWidth());
-      map.putDouble("height", image.getHeight());
-
-      promise.resolve(map);
-
-    } catch (Exception e) {
-      Log.e("E_RNThumnail_ERROR", e.getMessage());
-      promise.reject("E_RNThumnail_ERROR", e);
+    public RNThumbnailModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
     }
-  }
+
+    public static Bitmap convert(String base64Str) throws IllegalArgumentException
+    {
+        byte[] decodedBytes = Base64.decode(
+                base64Str.substring(base64Str.indexOf(",")  + 1),
+                Base64.DEFAULT
+        );
+
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+    }
+
+    public static String convert(Bitmap bitmap)
+    {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+
+        return Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT);
+    }
+
+    @Override
+    public String getName() {
+        return "RNThumbnail";
+    }
+
+    @ReactMethod
+    public void get(String filePath, Promise promise) {
+        boolean isLocal = filePath.contains("file://");
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        if (isLocal) {
+            filePath = filePath.replace("file://", "");
+            retriever.setDataSource(filePath);
+        } else {
+            retriever.setDataSource(filePath, new HashMap<String, String>());
+        }
+        Bitmap image = retriever.getFrameAtTime(1000000, MediaMetadataRetriever.OPTION_CLOSEST_SYNC);
+
+        try {
+            WritableMap map = Arguments.createMap();
+
+            map.putString("uri", convert(image));
+            map.putDouble("width", image.getWidth());
+            map.putDouble("height", image.getHeight());
+
+            promise.resolve(map);
+
+        } catch (Exception e) {
+            Log.e("E_RNThumnail_ERROR", e.getMessage());
+            promise.reject("E_RNThumnail_ERROR", e);
+        }
+    }
 }
